a1a6eb8217000c229c97152efb46586a4d245839
MatthewSlim <matthew_slim@hotmail.com>
2018-04-20 15:44:00 +0100

    Test sub

--- assessment.js
+++ assessment.js
@@ -1,20 +1,114 @@
 
 function isPrime(number){
 
-}
-  
-function amountToCoins(amount, coins){
+    let prime = 8;
+    let x = prime / prime;
+    let y = prime / 1;
+    
+    let primes = ["2", "3", "4", "5", "6", "7" ,"8", "9"];
+    parseInt(primes);
+    
+    if (prime / primes % == 0)
+    console.log("Not a prime number");
+    else console.log("Prime Number");
+    
+
+    
+    if (x = 1)
+    console.log("Prime Number");
+    else console.log("Not a prime number");
+
+//let prime = 8
+//let one = prime/1
+//let two = prime/2
+//let three = prime/3
+//let four = prime/4
+//let five = prime/5
+
+
+//let answer = two
+//if (prime/answer%=0)
+//console.log("Not a prime");
+//else console.log("Prime number");
 
 }
+
+
+function amountToCoins(amount, coins){
+//let amount = '16';
+//let coin1 = 25;
+//let coin2 = 10;
+//let coin3 = 5;
+//let coin4 = 2;
+//let coin5 = 1;
+
+//if (amount > coin1);
+//console.log(coin1);
+
+//if (amount = coin1);
+//console.log(coin1);
+//if (amount = coin2);
+//console.log(coin1, coin2);
+//if (amount = coin3);
+//console.log(coin1, coin2, coin3);
+//if (amount = coin4);
+//console.log(coin1, coin2, coin3, coin4);
+//if (amount = coin5);
+//console.log(coin1, coin2, coin3, coin4, coin5);
+
+let coins = [25,10,5,2,1];
+let amount = 46;
+for(i=[0];i<coins.length; i++);
+let sum = i + i;
+console.log(i);
+} 
 
 function uniqueLetters(string){
-
+    let uniqueletters = ["h", "e", "l","l", "o"];
+    for (i=[0]; i<uniqueletters.length; i++);
+    let count = 0
+    let upd = count + 1
+    count = upd;
+    if(upd<1){
+          let newul = uniqueletters.splice(3,1);
+    console.log(uniqueletters);
+}
 }
 
 function factorial(number){
+    let x = 5
 
+    for (x=5; x>0; --x)
+    sum = x * x ["x"];
+    console.log(x);
 }
+        //let x = 5
+      
+        //for (x=5; x>0; x--)
+        //sum = x * x;
+        //console.log(sum);
+       // sum = (x * x - 1)
 
 function fibonacci(number){
+    let uniqueletters = ["0", "1", "1","2", "3", "5", "8", "13", "21", "34"];
+    for (let x = 0; x>0; x++);
+    
+    
+    let fib1 = 0;
+    let fib2 = 1;
+    let fib3 = fib1 + fib2;
+    let fib4 = fib2 + fib3;
+    let fib5 = 3;
+    let fib6 = 5;
+    let fib7 = 8;
+    let fib8 = 13;
+    let fib9 = 21;
+    let fib10 = 34;
+    
+console.log(fib1, fib2, fib3, fib4);
+}
 
+    let i = 0;
+if (i=0; i>0; i++)
+document.write(["0"]+["1"]);
 }
